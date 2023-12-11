ad3b40f06100f4e882729014be963b8ec7d6f195
Trunggbebong <91788874+tranvantrung27@users.noreply.github.com>
2023-12-11 12:11:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var animation = lottie.loadAnimation({
     container: document.getElementById('lottie'), // the DOM element that will contain the animation
     renderer: 'svg',
     loop: false,
-    autoplay: true,
+    autoplay: false, // Đặt giá trị này thành false để animation không tự động chạy
     path: 'https://lottie.host/55c0c180-d778-4d34-954c-9038666cfb03/PQps4I3FYk.json' // the path to the animation JSON
 });
 
+// Thêm sự kiện click vào thẻ img
+document.getElementById('startAnimation').addEventListener('click', function() {
+    animation.play(); // Bắt đầu chạy animation khi người dùng nhấp vào thẻ img
+});
+
 animation.addEventListener('complete', function() {
-    // Tạo một trang HTML mới
-    var newPage = window.open("", "_self");
-    // Đặt URL của trang mới
-    newPage.document.location.href = "home.html";
+    // Chuyển hướng người dùng đến một trang khác sau khi hoàn thành animation
+    window.location.href = "home.html";
 });
